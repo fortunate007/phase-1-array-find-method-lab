cc75039f100dbc9529efe8b9fbe19ed39135f447
Newton <newtonkirimi55@gmail.com>
2024-07-05 00:32:03 -0700

    Using the Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// Function to find the year the Denver Broncos won the Superbowl
+function superbowlWin(record) {
+    // Use find() to locate the record with a win
+    const winRecord = record.find(game => game.result === "W");
+    
+    // Return the year of the win, or undefined if no win is found
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  // Export the function for use in tests
+  module.exports = superbowlWin;
+  
+
+
+
